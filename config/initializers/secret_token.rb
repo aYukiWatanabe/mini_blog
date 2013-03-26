6037452719f2c39a238cf4dd2cc28734bc9ec1fa
OTA Daichi <daichi.ota@access-company.com>
2013-03-26 19:42:01 +0900

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniBlog::Application.config.secret_token = '6f67c39c6cdde5d88ae15825804c286b1e145a3fd120817a607b16a78c1ea4dfda1de0513e1d3be0ba4c059dbe8fffe4314b36b40753a0f1c77d27f3e7cf54c4'
