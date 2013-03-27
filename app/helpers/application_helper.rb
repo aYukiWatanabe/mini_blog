e1dc7c25d5ab4328bc6dd1190792c887220b2eaa
OTA Daichi <daichi.ota@access-company.com>
2013-03-27 21:35:31 +0900

    Modify View

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def replace_br(text)
+    h(text).gsub(/\r?\n/, '<br>').html_safe
+  end
+
+  def error_message(model)
+    I18n.t('errors.template.header', count: model.errors.size, model: model.class.model_name.human)
+  end
 end
