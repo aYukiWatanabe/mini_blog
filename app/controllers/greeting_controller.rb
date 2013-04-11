816c41cb6089d98f128e0b14ef136d6d13610fa3
OTA Daichi <daichi.ota@access-company.com>
2013-04-11 09:55:41 +0900

    Add file

---
+++ app/controllers/greeting_controller.rb
@@ -0,0 +1,3 @@
+class GreetingController < ApplicationController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ MiniBlog::Application.routes.draw do
   root to: redirect('/blogs')
 
   scope format: 'html' do
+
+    # mount proc { [200, {'Content-Type' => 'text/plain'}, ['Hello, world']] } => 'greet'
+    # get 'greet' => 'greeting#hello_world'
+
     resources :blogs do
       resources :comments, only: [:create, :edit, :update, :destroy]
     end
