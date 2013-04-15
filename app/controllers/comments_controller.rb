84d5fbe3098b672ecec3941b2b42a9e4cf950e24
aMasaki.Takahashi <Masaki.Takahashi11@access-company.com>
2013-04-15 18:51:38 +0900

    Delete getting comment logic

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,17 +2,35 @@ class CommentsController < ApplicationController
 
   def create
     raise NotImplementedError
+
+    redirect_to blog_path(blog)
+  rescue ActiveRecord::RecordInvalid => e
+    @comment = e.record
+    @blog = @comment.blog
+    render 'blogs/show', status: 400
   end
 
   def edit
     raise NotImplementedError
+    @blog    = nil
+    @comment = nil
+
+    render 'blogs/show'
   end
 
   def update
     raise NotImplementedError
+
+    redirect_to blog_path(blog)
+  rescue ActiveRecord::RecordInvalid => e
+    @comment = e.record
+    @blog = @comment.blog
+    render 'blogs/show', status: 400
   end
 
   def destroy
     raise NotImplementedError
+
+    redirect_to blog_path(blog)
   end
 end
