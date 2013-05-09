07843eb5724d3aa28743cecb30705833084f34d7
OTA Daichi <daichi.ota@access-company.com>
2013-05-09 10:53:12 +0900

    [auth] Create user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 MiniBlog::Application.routes.draw do
-
   # seee http://guides.rubyonrails.org/routing.html
 
   root to: redirect('/blogs')
 
   scope format: 'html' do
-
-    # mount proc { [200, {'Content-Type' => 'text/plain'}, ['Hello, world']] } => 'hello_world'
-
+    devise_for :users
     resources :blogs do
       resources :comments, only: [:create, :edit, :update, :destroy]
     end
