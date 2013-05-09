b51b98ccacc603de59971a57c253cbbd8c8a6805
OTA Daichi <daichi.ota@access-company.com>
2013-05-09 13:55:27 +0900

    Remove unnecessary files

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 MiniBlog::Application.routes.draw do
-
   # seee http://guides.rubyonrails.org/routing.html
 
   root to: redirect('/blogs')
 
   scope format: 'html' do
-
-    # mount proc { [200, {'Content-Type' => 'text/plain'}, ['Hello, world']] } => 'hello_world'
-
     resources :blogs do
       resources :comments, only: [:create, :edit, :update, :destroy]
     end
