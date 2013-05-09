34fa772bb326725dcf7471757f96a85acaf91e43
WATANABE Yuki <yuki.watanabe@access-company.com>
2013-05-09 20:43:22 +0900

    Add authentication for creating blog articles

--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -29,58 +29,66 @@ describe BlogsController do
     end
   end
 
-  describe '#new' do
-    subject { get :new }
-    it { should render_template(:new) }
-  end
-
-  describe '#edit' do
-    before { @blog = Blog.create!(title: 'title', body: 'body') }
-    subject { get :edit, id: @blog.id }
-    it { should render_template(:edit) }
-  end
-
-  describe '#create' do
-    it 'should create' do
-      expect {
-        post :create, blog: { title: 'title', body: 'body' }
-      }.to change { Blog.count }.by(1)
-      response.should redirect_to(action: :index)
+  context 'when user logged in' do
+    before do
+      sign_in User.create! email: 'user@example.com', password: 'password'
     end
 
-    it 'should fail' do
-      expect {
-        post :create, blog: { title: '' }
-      }.not_to change { Blog.count }
-      response.status.should == 400
+    describe '#new' do
+      subject { get :new }
+      it { should render_template(:new) }
     end
+
+    describe '#edit' do
+      before { @blog = Blog.create!(title: 'title', body: 'body') }
+      subject { get :edit, id: @blog.id }
+      it { should render_template(:edit) }
+    end
+
+    describe '#create' do
+      it 'should create' do
+        expect {
+          post :create, blog: { title: 'title', body: 'body' }
+        }.to change { Blog.count }.by(1)
+        response.should redirect_to(action: :index)
+      end
+
+      it 'should fail' do
+        expect {
+          post :create, blog: { title: '' }
+        }.not_to change { Blog.count }
+        response.status.should == 400
+      end
+    end
+
+    describe '#update' do
+      before { @blog = Blog.create!(title: 'title', body: 'body') }
+
+      it 'should update' do
+        expect {
+          post :update, id: @blog.id, blog: { title: 'new-title' }
+        }.to change { @blog.reload.title }.from('title').to('new-title')
+        response.should redirect_to blog_path(@blog.id)
+      end
+
+      it 'should fail, if title is blank' do
+        expect {
+          post :update, id: @blog.id, blog: { title: '' }
+        }.not_to change { @blog.reload.title }
+        response.status.should == 400
+      end
+    end
+
+    describe '#destroy' do
+      before { @blog = Blog.create!(title: 'title', body: 'body') }
+
+      it 'should destroy' do
+        expect {
+          delete :destroy, id: @blog.id
+        }.to change { Blog.count }.by(-1)
+      end
+    end
+
   end
 
-  describe '#update' do
-    before { @blog = Blog.create!(title: 'title', body: 'body') }
-
-    it 'should update' do
-      expect {
-        post :update, id: @blog.id, blog: { title: 'new-title' }
-      }.to change { @blog.reload.title }.from('title').to('new-title')
-      response.should redirect_to blog_path(@blog.id)
-    end
-
-    it 'should fail, if title is blank' do
-      expect {
-        post :update, id: @blog.id, blog: { title: '' }
-      }.not_to change { @blog.reload.title }
-      response.status.should == 400
-    end
-  end
-
-  describe '#destroy' do
-    before { @blog = Blog.create!(title: 'title', body: 'body') }
-
-    it 'should destroy' do
-      expect {
-        delete :destroy, id: @blog.id
-      }.to change { Blog.count }.by(-1)
-    end
-  end
 end
